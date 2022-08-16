6f04ec462dbe0a9e5649c2f464021f9875fcf5b6
Maureen Njihia <maureennjihia468@gmail.com>
2022-08-17 01:03:37 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function (num1=2000, num2=100) {
+    return num1 / num2;
+}
+
+const square = number => number * number;
+
+const add = (firstNumber, secondNumber) => firstNumber + secondNumber;
